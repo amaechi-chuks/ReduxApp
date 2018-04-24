1d7c732763e20d4ee09de1acda6d26c0c8f72be6
chuks <amaechichuks2000@yahoo.com>
2018-04-24 09:18:59 +0100

    inserted a class for styling the searchbox

--- src/searchBox.js
+++ src/searchBox.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 
-const searchBox = () => {
+const SearchBox = () => {
     return(
-        <div>
+        <div className='pa2 tc'>
        <input
+       className= 'pa3 ba br3 b--dark-blue bg-lightest-blue '
         type='search'
          placeholder='search robots' 
          />
          </div>
     );
 }
-export default searchBox;
+export default SearchBox;
