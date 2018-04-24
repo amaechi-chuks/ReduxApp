a1795034093e64a3d449cb372b30238f0770428d
chuks <amaechichuks2000@yahoo.com>
2018-04-24 10:16:12 +0100

    modified the size of the img file

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     const { name, username, email, id } = props;
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-8'>
-            <img src={`https://robohash.org/${id}?100x100`} alt='robot' />
+            <img src={`https://robohash.org/${id}?400x400`} alt='robot' />
             <div>
                 <p className='ttu'>{name}</p>
                 <p>{username}</p>
