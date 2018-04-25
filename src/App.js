9e7081ef42f671e0741222dfb1222ddeff54c456
chuks <amaechichuks2000@yahoo.com>
2018-04-25 21:57:52 +0100

    added a signup page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { Robots } from './Robots';
+import Signup from './Signup';
 
 
 const App = () => {
     return (
         <div>
             <h1 className='tc'>RoboFriends</h1>
+            <Signup />
             <SearchBox />
             <CardList Robots={Robots} />
         </div>
