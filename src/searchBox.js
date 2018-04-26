4f2a68527501cb97b97be159e8f0153541bce77f
chuks <amaechichuks2000@yahoo.com>
2018-04-26 01:22:40 +0100

    destructuring searcfield and searchChange

--- src/searchBox.js
+++ src/searchBox.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 
-const SearchBox = () => {
+const SearchBox = ({searchfield, SearchChange}) => {
     return(
         <div className='pa2 tc'>
        <input
        className= 'pa3 ba br3 b--dark-blue bg-lightest-blue '
         type='search'
          placeholder='search robots' 
+         onChange={SearchChange}
          />
          </div>
     );
