bc65ee1b3c7329f7d08235e8cb14644445f84ad5
chuks <amaechichuks2000@yahoo.com>
2018-04-26 01:21:17 +0100

    added react component for search field

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { Robots } from './Robots';
-import Signup from './Signup';
 
 
-const App = () => {
-    return (
-        <div>
-            <h1 className='tc'>RoboFriends</h1>
-            <Signup />
-            <SearchBox />
-            <CardList Robots={Robots} />
-        </div>
-    );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            Robots: Robots,
+            searchfield: ''
+        }
+  }
+  onSearchChange(event) {
+    console.log(event.target.value);
+}
+  
+    render() {
+        return (
+            <div>
+                <h1 className='tc'>RoboFriends</h1>
+                <SearchBox SearchChange={this.onSearchChange}/>
+                <CardList Robots={this.state.Robots} />
+
+            </div>
+        );
+    }
 }
 export default App;
